5049b485cfcbe959ae37092f6e3998d3174257e3
James Anderson <3142796+jtanderson312@users.noreply.github.com>
2022-03-12 18:25:51 -0500

    web react app update

--- apps/web/src/config.js
+++ apps/web/src/config.js
@@ -1,17 +1,16 @@
 const dev = {
-    API_URL: "localhost:5000"
+  API_URL: "localhost:8100",
 };
 
 const prod = {
-    API_URL: "example.com"
+  API_URL: "example.com",
 };
 
-
-const config = process.env.NODE_ENV === 'production' ? prod : dev;
+const config = process.env.NODE_ENV === "production" ? prod : dev;
 
 export default {
-    // Common
+  // Common
 
-    // 
-    ...config
-}
+  //
+  ...config,
+};
